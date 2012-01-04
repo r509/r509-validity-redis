2466c7fbbf8516888e9717be1da96f8a361722b8
Paul Kehrer <paul.l.kehrer@gmail.com>
2012-01-04 10:00:56 -0600

    added issuer fingerprint support (will require r509 0.6)

--- lib/r509/Validity/Redis/Writer.rb
+++ lib/r509/Validity/Redis/Writer.rb
@@ -7,32 +7,32 @@ module R509::Validity::Redis
             @redis = redis
         end
 
-        def issue(serial)
-            raise ArgumentError.new("Serial must be provided") if serial.nil? or serial.to_s.empty?
-            cert = @redis.hgetall("cert:#{serial}")
+        def issue(issuer_fingerprint, serial)
+            raise ArgumentError.new("Serial and issuer fingerprint must be provided") if serial.to_s.empty? or issuer_fingerprint.to_s.empty?
+            cert = @redis.hgetall("cert:#{issuer_fingerprint}:#{serial}")
             if cert.nil? or not cert.has_key?("status")
-                @redis.hmset("cert:#{serial}", "status", 0)
+                @redis.hmset("cert:#{issuer_fingerprint}:#{serial}", "status", 0)
             else
-                raise StandardError.new("Serial #{serial} is already present")
+                raise StandardError.new("Serial #{serial} for issuer #{issuer_fingerprint} is already present")
             end
         end
 
-        def revoke(serial, revocation_time=Time.now.to_i, reason=0)
-            raise ArgumentError.new("Serial must be provided") if serial.nil? or serial.to_s.empty?
-            @redis.hmset("cert:#{serial}", 
-                "status", 1, 
-                "revocation_time", revocation_time || Time.now.to_i, 
+        def revoke(issuer_fingerprint, serial, revocation_time=Time.now.to_i, reason=0)
+            raise ArgumentError.new("Serial and issuer fingerprint must be provided") if serial.to_s.empty? or issuer_fingerprint.to_s.empty?
+            @redis.hmset("cert:#{issuer_fingerprint}:#{serial}",
+                "status", 1,
+                "revocation_time", revocation_time || Time.now.to_i,
                 "revocation_reason", reason || 0
             )
         end
 
-        def unrevoke(serial)
-            raise ArgumentError.new("Serial must be provided") if serial.nil? or serial.to_s.empty?
-            cert = @redis.hgetall("cert:#{serial}")
+        def unrevoke(issuer_fingerprint, serial)
+            raise ArgumentError.new("Serial and issuer fingerprint must be provided") if serial.to_s.empty? or issuer_fingerprint.to_s.empty?
+            cert = @redis.hgetall("cert:#{issuer_fingerprint}:#{serial}")
             if cert.nil? or not cert.has_key?("status")
-                raise StandardError.new("Serial #{serial} is not present")
+                raise StandardError.new("Serial #{serial} for issuer #{issuer_fingerprint} is not present")
             else
-                @redis.hmset("cert:#{serial}", "status", 0)
+                @redis.hmset("cert:#{issuer_fingerprint}:#{serial}", "status", 0)
             end
         end
     end
--- spec/writer_spec.rb
+++ spec/writer_spec.rb
@@ -8,104 +8,104 @@ describe R509::Validity::Redis::Writer do
     end
 
     context "issue" do
-        it "when serial is nil" do
+        it "when issuer fingerprint is nil/empty string" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            expect { writer.issue(nil) }.to raise_error(ArgumentError, "Serial must be provided")
+            expect { writer.issue(nil,123) }.to raise_error(ArgumentError, "Serial and issuer fingerprint must be provided")
         end
-        it "when serial is empty string" do
+        it "when serial is nil/empty string" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            expect { writer.issue("") }.to raise_error(ArgumentError, "Serial must be provided")
+            expect { writer.issue("abcdef",nil) }.to raise_error(ArgumentError, "Serial and issuer fingerprint must be provided")
         end
-        it "when serial is provided (check returns nil)" do
+        it "when serial/issuer is provided (check returns nil)" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            redis.should_receive(:hgetall).with("cert:123").and_return(nil)
-            redis.should_receive(:hmset).with("cert:123", "status", 0)
-            writer.issue(123)
+            redis.should_receive(:hgetall).with("cert:abcdef:123").and_return(nil)
+            redis.should_receive(:hmset).with("cert:abcdef:123", "status", 0)
+            writer.issue("abcdef",123)
         end
-        it "when serial is provided (check returns {})" do
+        it "when serial/issuer is provided (check returns {})" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            redis.should_receive(:hgetall).with("cert:123").and_return({})
-            redis.should_receive(:hmset).with("cert:123", "status", 0)
-            writer.issue(123)
+            redis.should_receive(:hgetall).with("cert:abcdef:123").and_return({})
+            redis.should_receive(:hmset).with("cert:abcdef:123", "status", 0)
+            writer.issue("abcdef",123)
         end
-        it "when serial is already present" do
+        it "when serial/issuer is already present" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            redis.should_receive(:hgetall).with("cert:123").and_return({"status"=>0})
-            expect { writer.issue(123) }.to raise_error(StandardError, "Serial 123 is already present")
+            redis.should_receive(:hgetall).with("cert:abcdef:123").and_return({"status"=>0})
+            expect { writer.issue("abcdef",123) }.to raise_error(StandardError, "Serial 123 for issuer abcdef is already present")
         end
     end
 
     context "revoke" do
-        it "when serial is nil" do
+        it "when issuer fingerprint is nil/empty string" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            expect { writer.revoke(nil) }.to raise_error(ArgumentError, "Serial must be provided")
+            expect { writer.revoke(nil,123) }.to raise_error(ArgumentError, "Serial and issuer fingerprint must be provided")
         end
-        it "when serial is empty string" do
+        it "when serial is nil/empty string" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            expect { writer.revoke("") }.to raise_error(ArgumentError, "Serial must be provided")
+            expect { writer.revoke("abcdef",nil) }.to raise_error(ArgumentError, "Serial and issuer fingerprint must be provided")
         end
         it "when time and reason aren't provided" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            redis.should_receive(:hmset).with("cert:123", "status", 1, "revocation_time", Time.now.to_i, "revocation_reason", 0)
-            writer.revoke(123)
+            redis.should_receive(:hmset).with("cert:abcdef:123", "status", 1, "revocation_time", Time.now.to_i, "revocation_reason", 0)
+            writer.revoke("abcdef",123)
         end
         it "when time and reason are nil" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            redis.should_receive(:hmset).with("cert:123", "status", 1, "revocation_time", Time.now.to_i, "revocation_reason", 0)
-            writer.revoke(123, nil, nil)
+            redis.should_receive(:hmset).with("cert:abcdef:123", "status", 1, "revocation_time", Time.now.to_i, "revocation_reason", 0)
+            writer.revoke("abcdef",123, nil, nil)
         end
         it "when time is provided, but not reason" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            redis.should_receive(:hmset).with("cert:123", "status", 1, "revocation_time", 100, "revocation_reason", 0)
-            writer.revoke(123, 100)
+            redis.should_receive(:hmset).with("cert:abcdef:123", "status", 1, "revocation_time", 100, "revocation_reason", 0)
+            writer.revoke("abcdef",123, 100)
         end
         it "when time and reason are provided" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            redis.should_receive(:hmset).with("cert:123", "status", 1, "revocation_time", 100, "revocation_reason", 2)
-            writer.revoke(123, 100, 2)
+            redis.should_receive(:hmset).with("cert:abcdef:123", "status", 1, "revocation_time", 100, "revocation_reason", 2)
+            writer.revoke("abcdef",123, 100, 2)
         end
     end
 
     context "unrevoke" do
-        it "when serial is nil" do
+        it "when issuer fingerprint is nil/empty string" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            expect { writer.unrevoke(nil) }.to raise_error(ArgumentError, "Serial must be provided")
+            expect { writer.unrevoke(nil,123) }.to raise_error(ArgumentError, "Serial and issuer fingerprint must be provided")
         end
-        it "when serial is empty string" do
+        it "when serial is nil/empty string" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            expect { writer.unrevoke("") }.to raise_error(ArgumentError, "Serial must be provided")
+            expect { writer.unrevoke("abcdef",nil) }.to raise_error(ArgumentError, "Serial and issuer fingerprint must be provided")
         end
-        it "when serial is provided" do
+        it "when serial/issuer is provided" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            redis.should_receive(:hgetall).with("cert:123").and_return({"status" => 1})
-            redis.should_receive(:hmset).with("cert:123", "status", 0)
-            writer.unrevoke(123)
+            redis.should_receive(:hgetall).with("cert:abcdef:123").and_return({"status" => 1})
+            redis.should_receive(:hmset).with("cert:abcdef:123", "status", 0)
+            writer.unrevoke("abcdef",123)
         end
         it "when cert record doesn't exist (nil)" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            redis.should_receive(:hgetall).with("cert:123").and_return(nil)
-            expect { writer.unrevoke(123) }.to raise_error(StandardError, "Serial 123 is not present")
+            redis.should_receive(:hgetall).with("cert:abcdef:123").and_return(nil)
+            expect { writer.unrevoke("abcdef",123) }.to raise_error(StandardError, "Serial 123 for issuer abcdef is not present")
         end
         it "when cert record doesn't exist ({})" do
             redis = double("redis")
             writer = R509::Validity::Redis::Writer.new(redis)
-            redis.should_receive(:hgetall).with("cert:123").and_return({})
-            expect { writer.unrevoke(123) }.to raise_error(StandardError, "Serial 123 is not present")
+            redis.should_receive(:hgetall).with("cert:abcdef:123").and_return({})
+            expect { writer.unrevoke("abcdef",123) }.to raise_error(StandardError, "Serial 123 for issuer abcdef is not present")
         end
     end
 end
