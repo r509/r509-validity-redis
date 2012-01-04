9b50a95cb0556a85c634b4b20e0ba00862bba936
Paul Kehrer <paul.l.kehrer@gmail.com>
2012-01-04 11:48:04 -0600

    rename issuer_fingerprint to just issuer to reflect its updated role

--- lib/r509/Validity/Redis/Writer.rb
+++ lib/r509/Validity/Redis/Writer.rb
@@ -7,32 +7,32 @@ module R509::Validity::Redis
             @redis = redis
         end
 
-        def issue(issuer_fingerprint, serial)
-            raise ArgumentError.new("Serial and issuer fingerprint must be provided") if serial.to_s.empty? or issuer_fingerprint.to_s.empty?
-            cert = @redis.hgetall("cert:#{issuer_fingerprint}:#{serial}")
+        def issue(issuer, serial)
+            raise ArgumentError.new("Serial and issuer must be provided") if serial.to_s.empty? or issuer.to_s.empty?
+            cert = @redis.hgetall("cert:#{issuer}:#{serial}")
             if cert.nil? or not cert.has_key?("status")
-                @redis.hmset("cert:#{issuer_fingerprint}:#{serial}", "status", 0)
+                @redis.hmset("cert:#{issuer}:#{serial}", "status", 0)
             else
-                raise StandardError.new("Serial #{serial} for issuer #{issuer_fingerprint} is already present")
+                raise StandardError.new("Serial #{serial} for issuer #{issuer} is already present")
             end
         end
 
-        def revoke(issuer_fingerprint, serial, revocation_time=Time.now.to_i, reason=0)
-            raise ArgumentError.new("Serial and issuer fingerprint must be provided") if serial.to_s.empty? or issuer_fingerprint.to_s.empty?
-            @redis.hmset("cert:#{issuer_fingerprint}:#{serial}",
+        def revoke(issuer, serial, revocation_time=Time.now.to_i, reason=0)
+            raise ArgumentError.new("Serial and issuer must be provided") if serial.to_s.empty? or issuer.to_s.empty?
+            @redis.hmset("cert:#{issuer}:#{serial}",
                 "status", 1,
                 "revocation_time", revocation_time || Time.now.to_i,
                 "revocation_reason", reason || 0
             )
         end
 
-        def unrevoke(issuer_fingerprint, serial)
-            raise ArgumentError.new("Serial and issuer fingerprint must be provided") if serial.to_s.empty? or issuer_fingerprint.to_s.empty?
-            cert = @redis.hgetall("cert:#{issuer_fingerprint}:#{serial}")
+        def unrevoke(issuer, serial)
+            raise ArgumentError.new("Serial and issuer must be provided") if serial.to_s.empty? or issuer.to_s.empty?
+            cert = @redis.hgetall("cert:#{issuer}:#{serial}")
             if cert.nil? or not cert.has_key?("status")
-                raise StandardError.new("Serial #{serial} for issuer #{issuer_fingerprint} is not present")
+                raise StandardError.new("Serial #{serial} for issuer #{issuer} is not present")
             else
-                @redis.hmset("cert:#{issuer_fingerprint}:#{serial}", "status", 0)
+                @redis.hmset("cert:#{issuer}:#{serial}", "status", 0)
             end
         end
     end
